635a3b0385ad412312a9209bde788d8679cb8c04
backas36 <backas36@gmail.com>
2025-06-03 00:49:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# auth_gorouter_riverpod
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/ashi/auth_gorouter_riverpod/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ashi.auth_gorouter_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
